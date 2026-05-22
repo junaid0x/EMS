911e8bb1bc50aa13b85aeb1d5adcfbe7fb5ca081
Junaid <connectjd10@gmail.com>
2026-05-22 21:42:53 +0500

    Added Pages and Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import {Toaster } from "react-hot-toast"
+import { Navigate, Route, Routes } from "react-router-dom"
+import LoginLanding from "./pages/LoginLanding"
+import Layout from "./pages/Layout"
+import Dashboard from "./pages/Dashboard"
+import Employess from "./pages/Employees"
+import Attendence from "./pages/Attendence"
+import Leave from "./pages/Leave"
+import Settings from "./pages/Settings"
+import Payslips from "./pages/Payslips"
+import PrintPaySlip from "./pages/PrintPaySlip"
+
 
 const App = () => {
   return (
-    <div className='text-3xl font-bold underline'>App</div>
+    <>
+      <Toaster/>
+      <Routes>
+          <Route path="/login" element={ <LoginLanding/>} />
+          <Route element={<Layout/>}>
+              <Route path="/dashboard" element={<Dashboard/>}/>
+              <Route path="/employees" element={<Employees/>}/>
+              <Route path="/attendence" element={<Attendence/>}/>
+              <Route path="/leave" element={<Leave/>}/>
+              <Route path="/payslips" element={<Payslips/>}/>
+              <Route path="/settings" element={<Settings/>}/>
+          </Route>
+          <Route path="/print/payslips/:id" element={<PrintPaySlip/>}/>
+          <Route path="*" element={<Navigate to="/dashboard" replace/>}/>
+      </Routes>
+    </>
   )
 }
 
-export default App
+export default App 
