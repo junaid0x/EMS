0dd65ca7f1d5b4072e20df45ddfbe099a0142763
Junaid <connectjd10@gmail.com>
2026-05-24 05:03:03 +0500

    Added Components and Created Login Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,13 @@ import { Navigate, Route, Routes } from "react-router-dom"
 import LoginLanding from "./pages/LoginLanding"
 import Layout from "./pages/Layout"
 import Dashboard from "./pages/Dashboard"
-import Employess from "./pages/Employees"
+import Employees from "./pages/Employees"
 import Attendence from "./pages/Attendence"
 import Leave from "./pages/Leave"
 import Settings from "./pages/Settings"
 import Payslips from "./pages/Payslips"
 import PrintPaySlip from "./pages/PrintPaySlip"
+import LoginForm from "./components/LoginForm"
 
 
 const App = () => {
@@ -17,11 +18,14 @@ const App = () => {
       <Toaster/>
       <Routes>
           <Route path="/login" element={ <LoginLanding/>} />
+          <Route path="/login/admin" element={ <LoginForm role="admin" title="Admin Portal" subtitle="Sign in to manage the organization"/>} />
+          <Route path="/login/employee" element={ <LoginForm role="employee" title="Employee Portal" subtitle="Sign in to access your account"/>} />
+
           <Route element={<Layout/>}>
               <Route path="/dashboard" element={<Dashboard/>}/>
               <Route path="/employees" element={<Employees/>}/>
               <Route path="/attendence" element={<Attendence/>}/>
-              <Route path="/leave" element={<Leave/>}/>
+              <Route path="/leave" element={<Leave/>}/> 
               <Route path="/payslips" element={<Payslips/>}/>
               <Route path="/settings" element={<Settings/>}/>
           </Route>
