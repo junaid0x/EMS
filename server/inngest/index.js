eb1b11d062c5b986df573bbb9d489af36f4ab161
Junaid <connectjd10@gmail.com>
2026-06-19 04:26:23 +0500

    fix: update cron schedule for attendance reminder to reflect IST timezone

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -94,7 +94,7 @@ const leaveApplicationReminder = inngest.createFunction(
 
 // Cron: Check attendance at 11:30 AM and email absent employee
 const attendanceReminderCron = inngest.createFunction(
-  { id: "attendance-reminder-cron", triggers: [{cron: '0 0 6 * * *'}]}, 
+  { id: "attendance-reminder-cron", triggers: [{cron: 'TZ=Asia/Kolkata 30 11 * * *'}]}, 
    //06:00 UTC = 11:30 AM IST
     async ({step}) => {
       //Step 1: get today's date range (IST)
